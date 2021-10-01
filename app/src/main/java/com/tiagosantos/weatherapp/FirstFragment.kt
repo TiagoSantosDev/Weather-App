4d25df37d310e7f5099c72d2c22ce253697d40fc
TiagoSantosDev <up201808919@fe.up.pt>
2021-10-01 13:57:19 +0100

    Added Retrofit basic structure.

--- app/src/main/java/com/tiagosantos/weatherapp/FirstFragment.kt
+++ app/src/main/java/com/tiagosantos/weatherapp/FirstFragment.kt
@@ -1,12 +1,10 @@
 package com.tiagosantos.weatherapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -14,8 +12,9 @@ import androidx.navigation.fragment.findNavController
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
@@ -23,9 +22,5 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 }
---
+++ app/src/main/java/com/tiagosantos/weatherapp/service/ApiInterface.kt
@@ -0,0 +1,10 @@
+package com.tiagosantos.weatherapp.service
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiInterface {
+    //@GET("weather")
+    //fun callApi(@Query("id") cityId: Int):
+}
