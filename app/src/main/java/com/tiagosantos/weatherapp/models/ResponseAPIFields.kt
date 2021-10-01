968c1e29b52c43cbc1abc5cf79d65ad5b08f62d7
TiagoSantosDev <up201808919@fe.up.pt>
2021-10-01 16:45:09 +0100

    Added model needed for conversion of received JSON into usable data structure.

---
+++ app/src/main/java/com/tiagosantos/weatherapp/models/ResponseAPIFields.kt
@@ -0,0 +1,61 @@
+package com.tiagosantos.weatherapp.models
+
+data class CurrentWeather(
+    var locationName: String,
+    var currentWeather: String,
+    var currentTemp: Double
+)
+
+// locationName refers to the city Name
+// currentWeather refers to the main field on the JSON response
+
+/*
+Mockup API response for reference
+
+{
+  "coord": {
+    "lon": -122.08,
+    "lat": 37.39
+  },
+  "weather": [
+    {
+      "id": 800,
+      "main": "Clear",
+      "description": "clear sky",
+      "icon": "01d"
+    }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 282.55,
+    "feels_like": 281.86,
+    "temp_min": 280.37,
+    "temp_max": 284.26,
+    "pressure": 1023,
+    "humidity": 100
+  },
+  "visibility": 16093,
+  "wind": {
+    "speed": 1.5,
+    "deg": 350
+  },
+  "clouds": {
+    "all": 1
+  },
+  "dt": 1560350645,
+  "sys": {
+    "type": 1,
+    "id": 5122,
+    "message": 0.0139,
+    "country": "US",
+    "sunrise": 1560343627,
+    "sunset": 1560396563
+  },
+  "timezone": -25200,
+  "id": 420006353,
+  "name": "Mountain View",
+  "cod": 200
+  }
+  
+  
+ */
